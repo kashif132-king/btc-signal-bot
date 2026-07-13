09c352aa32ee7284fbe9e6f42a7dad9363cbf30f
kashif132-king <kashifali000132@gmail.com>
2026-07-13 17:30:19 +0500

    Add imports and environment variable setup

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("BTC Signal Bot Started")
+import os
+import time
+import requests
+import pandas as pd
+import ta
+
+from binance.client import Client
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
+
+SYMBOL = "BTCUSDT"
+TIMEFRAME = "5m"
+
+client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
+bot = Bot(token=BOT_TOKEN)
